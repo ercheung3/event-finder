bfbb7c3b87d957ff39a704756fc31c281d8edda2
BrevanBender <bendercoding@gmail.com>
2022-03-14 16:11:30 -0700

    this may need to go in the app.js also fixed class to agreed upon term

--- middleware/likeLogic.js
+++ middleware/likeLogic.js
@@ -1,6 +1,19 @@
-let buttons = document.querySelectorAll('.likes');
+let buttons = document.querySelectorAll('.likes-btn');
+//every button with class of likes
 for(let button of buttons){
+    //get each button and add event listener
     button.addEventListener('click', ()=>{
-        
+    //make sure user in logged in
+    if (req.session.isLoggedIn) {
+        //find event by id which should be the same as the button
+        const thisEvent = Event.findById(button.id)
+        //but the current user and add the like
+        const currentUser = await User.findByIdAndUpdate(req.session.userId,{
+            $push:{likes:thisEvent}
+        })
+        //if no one is logged in don't do all that
+      } else {
+        alert("You must be logged in to Like events")
+      }
     })
 }
